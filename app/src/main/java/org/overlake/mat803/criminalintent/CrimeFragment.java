5a243635062f30a71feefad9face8308e326ef20
David Basham <dbasham@overlake.org>
2020-02-12 10:45:57 -0800

    Finish chapter 7

--- app/src/main/java/org/overlake/mat803/criminalintent/CrimeFragment.java
+++ app/src/main/java/org/overlake/mat803/criminalintent/CrimeFragment.java
@@ -1,18 +1,30 @@
 package org.overlake.mat803.criminalintent;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static android.widget.CompoundButton.*;
+
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
-
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
+    public static final String TAG = "CrimeFragment";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +34,45 @@ public class CrimeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_crime, container, false);
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = v.findViewById(R.id.crime_title);
+
+        mTitleField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d(TAG, "beforeTextChanged called:" + charSequence);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+
+                Log.d(TAG, "onTextChanged called:" + charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!editable.toString().endsWith("!")){
+                    editable.append("!");
+                }
+                Log.d(TAG, "afterTextChanged called.");
+            }
+        });
+
+        mDateButton = v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckbox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(b);
+            }
+        });
+
+        return v;
     }
 }
