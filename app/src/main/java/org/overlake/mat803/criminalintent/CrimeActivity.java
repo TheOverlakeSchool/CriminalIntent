25f90ddb3e7ec77c322f0553d2b9c2d2cb9de4b9
David Basham <dbasham@overlake.org>
2020-02-13 13:55:30 -0800

    set base for chapter 8

--- app/src/main/java/org/overlake/mat803/criminalintent/CrimeActivity.java
+++ app/src/main/java/org/overlake/mat803/criminalintent/CrimeActivity.java
@@ -1,17 +1,17 @@
 package org.overlake.mat803.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class CrimeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
