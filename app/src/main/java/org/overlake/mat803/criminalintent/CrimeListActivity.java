52a8d81d6e471eaf685f84297d3a60003836015b
David Basham <dbasham@overlake.org>
2020-03-21 10:25:37 -0700

    Moves launcher title to CrimeListActivity

--- app/src/main/java/org/overlake/mat803/criminalintent/CrimeListActivity.java
+++ app/src/main/java/org/overlake/mat803/criminalintent/CrimeListActivity.java
@@ -1,8 +1,16 @@
 package org.overlake.mat803.criminalintent;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.crime_list_title);
+    }
 
     @Override
     protected Fragment createFragment() {
